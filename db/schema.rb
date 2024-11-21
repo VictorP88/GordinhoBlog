bcd69560feaf6f70be6eea7134a5d6f3dc15b5eb
Victor Pinheiro <victorpinheirojc@gmail.com>
2024-11-21 14:57:35 -0300

    Scheduled and draft posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_19_173000) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_21_163041) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.string "string"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
