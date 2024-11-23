c42d54f5ad8fa888c8a02b46b83d6a733c3c0e27
Victor Pinheiro <victorpinheirojc@gmail.com>
2024-11-23 13:41:52 -0300

    Upload and manage cover images

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], mmodule: :blog_posts
+  end
+
   root "blog_posts#index"
 end
