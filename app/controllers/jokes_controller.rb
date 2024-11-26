c1f8ce4c3c911060f2ca19667076a5af790fec80
Victor Pinheiro <victorpinheirojc@gmail.com>
2024-11-26 15:54:05 -0300

    alguns ajustes do Chuck

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -3,13 +3,13 @@ class JokesController < ApplicationController
   base_uri 'https://api.chucknorris.io'
 
   def random_joke
-    Rails.logger.info("Random joke hit") # Log para confirmação
     response = self.class.get('/jokes/random')
     @joke = response.parsed_response['value']
-    
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to root_path }
-    end
+
+    # Armazenando a piada na sessão
+    session[:joke] = @joke
+
+    # Redireciona para a página inicial
+    redirect_to root_path
   end
 end
